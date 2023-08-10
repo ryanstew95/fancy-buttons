a319ea37ecee883bd4281758d2119b7499da146f
Ryan Stewart <ryanstew17@gmail.com>
2023-08-10 17:13:42 -0300

    added dark mode to light button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
+import { useState } from 'react';
 import AngryButton from './components/AngryButton';
 import CounterButton from './components/CounterButton';
 import LightSwitchButton from './components/LightSwitchButton';
 import TextRepeaterButton from './components/TextRepeaterButton';
 function App() {
+  const [light, setLight] = useState('off');
+  const dark = (light === 'off') ? 'dark' : '';
   return (
-    <div className="App">
+    <div className={`App ${dark}`}>
       <h1>Fancy Buttons!</h1>
 <AngryButton />
 <CounterButton />
-<LightSwitchButton />
+<LightSwitchButton light={light} setLight={setLight}/>
 <TextRepeaterButton />
     </div>
   );
