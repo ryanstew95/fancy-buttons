fc1922389c26908f940bbe058063be08790eedfc
Ryan Stewart <ryanstew17@gmail.com>
2023-08-10 20:11:35 -0300

    working on angry button

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import TextRepeaterButton from './components/TextRepeaterButton';
 function App() {
   const [light, setLight] = useState('off');
   const dark = (light === 'off') ? 'dark' : '';
+  const switchLight = () => setLight((light === 'on') ? 'off' : 'on'); 
   return (
+    
     <div className={`App ${dark}`}>
       <h1>Fancy Buttons!</h1>
 <AngryButton />
 <CounterButton />
-<LightSwitchButton light={light} setLight={setLight}/>
+<LightSwitchButton light={light} switchLight={switchLight}/>
 <TextRepeaterButton />
     </div>
   );
--- src/components/LightSwitchButton.jsx
+++ src/components/LightSwitchButton.jsx
@@ -1,8 +1,8 @@
 function LightSwitchButton(props) {
-  const {light, setLight} = props;
-  const handleClick = () => setLight(light === "on" ? "off" : "on");
+  const { light, switchLight } = props;
+  const handleClick = () => switchLight();
   return (
-<button onClick={handleClick} className="LightSwitchButton">
+<button onClick={switchLight} className="LightSwitchButton">
 {light === "on" && <span><i>💡</i> I'm on!</span>}
 {light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
 </button>
